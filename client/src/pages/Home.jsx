e20b6729e19156c36b321716c1a79cddf26f63cc
Rakhsrb <suhrobrahmatullayev973132@gmail.com>
2024-09-16 16:35:25 +0500

    client tarafni yozish boshlandi

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,25 +1,7 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from 'react'
 
 export const Home = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    async function getData() {
-      const { data } = (await axios.get("http://localhost:8000/product")).data;
-      setData(data);
-    }
-    getData();
-  }, []);
-
-  console.log(data);
-
   return (
-    <div>
-      <h1>{data[0]?.title}</h1>
-      <p>{data[0]?.description}</p>
-      <h2>{data[0]?.price.toLocaleString()}</h2>
-      <img src={data[0]?.previewPhoto} alt="" />
-    </div>
-  );
-};
+    <div>Home</div>
+  )
+}
