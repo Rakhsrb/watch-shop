7bd68d06f99758f46edfe28b5b7f23003a60bbee
Rakhsrb <suhrobrahmatullayev973132@gmail.com>
2024-09-20 15:01:57 +0500

    asdsad

--- client/src/components/shared/Header/Header.jsx
+++ client/src/components/shared/Header/Header.jsx
@@ -12,7 +12,6 @@ export const Header = () => {
   document.body.style.overflowY = navActive ? "hidden" : "auto";
 
   useEffect(() => {
-    ё;
     document.addEventListener("click", (e) => {
       if (
         navMenuRef.current &&
@@ -35,43 +34,45 @@ export const Header = () => {
             alt="Logo"
           />
         </Link>
-        <ul
-          ref={navMenuRef}
-          className={`flex gap-5 font-semibold text-white nav-menu ${
-            navActive ? "active" : ""
-          }`}
-        >
-          <li>
-            <a href="#watches">Watches</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#features">Features</a>
-          </li>
-        </ul>
-        <div className="flex items-center gap-4 text-white">
-          <Link to={"/cart"} className="font-bold">
-            <ShoppingBag size={25} />
-          </Link>
-          <div className="handleNav">
-            {navActive ? (
-              <X
-                ref={toggleButtonRef}
-                size={25}
-                onClick={() => setNavActive(false)}
-              />
-            ) : (
-              <List
-                ref={toggleButtonRef}
-                size={25}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setNavActive(true);
-                }}
-              />
-            )}
+        <div className="flex gap-8 items-center">
+          <ul
+            ref={navMenuRef}
+            className={`flex gap-5 font-semibold text-white nav-menu ${
+              navActive ? "active" : ""
+            }`}
+          >
+            <li>
+              <a href="#watches">Watches</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#features">Features</a>
+            </li>
+          </ul>
+          <div className="flex items-center gap-4 text-white">
+            <Link to={"/cart"} className="font-bold">
+              <ShoppingBag size={25} />
+            </Link>
+            <div className="handleNav">
+              {navActive ? (
+                <X
+                  ref={toggleButtonRef}
+                  size={25}
+                  onClick={() => setNavActive(false)}
+                />
+              ) : (
+                <List
+                  ref={toggleButtonRef}
+                  size={25}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setNavActive(true);
+                  }}
+                />
+              )}
+            </div>
           </div>
         </div>
       </Container>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,21 +1,74 @@
 import React from "react";
 import { Container } from "../../components/shared/Container/Container";
 import { Link } from "react-router-dom";
+import { ArrowRight } from "@phosphor-icons/react";
 
 export const Home = () => {
   return (
     <>
-      <section className="h-screen bg-cover bg-[url('https://media.rolex.com/image/upload/v1725888690/rolexcom/collection/configurator/config-launcher/2024/config-launcher-watches-day-date-m228235-0055_2403jva_002.jpg')]">
-        <Container className={"grid grid-cols-1 lg:grid-cols-2 h-full"}>
-          <div className="flex flex-col h-full justify-center gap-10">
-            <h1>Discover Luxurious Watch Which You Will Love.</h1>
-            <p>
+      <section className="h-screen hero_bg">
+        <Container
+          className={
+            "grid grid-cols-1 lg:grid-cols-2 h-full items-center gap-12"
+          }
+        >
+          <div className="flex flex-col h-full justify-center items-start gap-10">
+            <h1 className="text-white text-6xl font-bold">
+              Discover Luxurious Watch Which You Will Love.
+            </h1>
+            <p className="text-[#B1B1B1] font-light">
               What looked like a small patch of purple grass, above five feet
               square, was moving across the sand in their direction. Almost do
               am or limits hearts. Resolve parties but why she shewing. She sang
               know now how nay cold real case.
             </p>
-            <Link>Read more</Link>
+            <Link className="text-[#9A836C] border-2 border-[#9A836C] text-2xl py-3 px-4 flex items-center justify-center gap-4">
+              Read more
+              <ArrowRight />
+            </Link>
+          </div>
+        </Container>
+      </section>
+      <section className="py-20 bg-[#1B1D22]">
+        <Container>
+          <h1 className="mb-10 text-white text-3xl">New Watches</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="text-white">
+              <figure className="w-full overflow-hidden">
+                <img
+                  className="w-full object-cover"
+                  src="https://magazine.chrono24.com/cdn-cgi/image/f=auto,metadata=none,fit=cover,q=65,w=1190,h=595,dpr=2.0/2023/01/Omega-2-1-1.jpg"
+                  alt=""
+                />
+              </figure>
+              <h3 className="">Thermo Ball Etip Gloves</h3>
+              <h2>$ 45,743</h2>
+            </div>
+            <div className="text-white">
+              <figure className="w-full overflow-hidden">
+                <img
+                  className="w-full object-cover"
+                  src="https://magazine.chrono24.com/cdn-cgi/image/f=auto,metadata=none,fit=cover,q=65,w=1190,h=595,dpr=2.0/2023/01/Omega-2-1-1.jpg"
+                  alt=""
+                />
+              </figure>
+              <h3 className="">Thermo Ball Etip Gloves</h3>
+              <h2>$ 45,743</h2>
+            </div>
+            <div className="text-white">
+              <figure className="w-full overflow-hidden">
+                <img
+                  className="w-full object-cover"
+                  src="https://magazine.chrono24.com/cdn-cgi/image/f=auto,metadata=none,fit=cover,q=65,w=1190,h=595,dpr=2.0/2023/01/Omega-2-1-1.jpg"
+                  alt=""
+                />
+              </figure>
+              <h3 className="">Thermo Ball Etip Gloves</h3>
+              <h2>$ 45,743</h2>
+            </div>
+          </div>
+          <div className="flex items-center justify-center my-20">
+            <button className="">View more</button>
           </div>
         </Container>
       </section>
